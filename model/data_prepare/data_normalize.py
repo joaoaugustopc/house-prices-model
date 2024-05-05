4020d88b18a3d66dc0d081da0e1103db7d3da668
Luíza Costa <117786812+luizamc003@users.noreply.github.com>
2024-05-05 17:31:01 -0300

    conserta encode

--- model/data_prepare/data_normalize.py
+++ model/data_prepare/data_normalize.py
@@ -2,11 +2,21 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
+#normalizar os dados com ordem e numericos - StandardScaler, sem ser one hot encoder
+
 data_train = pd.read_csv('dataset/train.csv')
 data_test = pd.read_csv('dataset/test.csv')
 
-X_train = data_train.drop(columns=['SalePrice','Id'])
+X_train = data_train.drop(columns=['SalePrice','Id', ])
 X_train = X_train.select_dtypes(include=["number"], exclude=["object"])
+
+#colunas binárias
+cols_to_drop = data_train.filter(regex='onehotencoder').columns
+print(cols_to_drop)
+data_train = data_train.drop(columns=cols_to_drop)
+# Fazer o mesmo para data_test
+cols_to_drop = data_test.filter(regex='onehotencoder').columns
+data_test = data_test.drop(columns=cols_to_drop)
 
 X_test = data_test.drop(columns=['Id'])
 X_test = X_test.select_dtypes(include=["number"], exclude=["object"])
--- model/data_prepare/encode_1.py
+++ model/data_prepare/encode_1.py
@@ -6,20 +6,19 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OrdinalEncoder
 
 
-data_train = pd.read_csv('dataset/train.csv')
-data_test = pd.read_csv('dataset/test.csv')
+data_train = pd.read_csv('dataset/train.csv', na_values=['NA'])
+data_test = pd.read_csv('dataset/test.csv', na_values=['NA'])
 y_train = data_train['SalePrice']
 data_train.drop(columns=['SalePrice'], inplace=True)
 
-# Lista de variáveis categóricas que possuem uma ordem natural
+# Lista de variáveis categóricas que possuem uma ordem natural padronizada
 list_ordinal = ['BsmtQual','BsmtCond','FireplaceQu','GarageQual','GarageCond'] # NA, Po, Fa, TA, Gd, Ex
 
 list_ordinal2 = ['ExterQual','ExterCond','HeatingQC','KitchenQual'] # Po, Fa, TA, Gd, Ex
 
 list_lb = ['CentralAir','Street'] # Sim ou não / Paved ou Gravel
 
-list = list_ordinal+ list_ordinal2 + list_lb + ['LotShape','LandSlope','BsmtExposure','BsmtFinType1','BsmtFinType2','GarageFinish','PavedDrive','Fence']
-
+list = list_ordinal+ list_ordinal2 + list_lb + ['LotShape','LandSlope','BsmtExposure','BsmtFinType1','BsmtFinType2','GarageFinish','PavedDrive']
 
 pre_data_train = data_train.drop(columns=list)
 pre_data_train = pre_data_train.select_dtypes(include=["object"]).columns
@@ -31,15 +30,14 @@ data_train[list] = data_train[list].fillna('NA')
 data_test[list] = data_test[list].fillna('NA')
 
 
-# Criar listas de categorias
+# Criar listas de categorias com ordem diferentes
 lotshape_categories = [['IR3', 'IR2', 'IR1', 'Reg']] #olhar
 landslope_categories = [['Sev', 'Mod', 'Gtl']]
 bsmtexposure_categories = [['NA', 'No', 'Mn', 'Av', 'Gd']]
 bsmtfintype_categories = [['NA', 'Unf', 'LwQ', 'Rec', 'BLQ', 'ALQ', 'GLQ']]
 garagefinish_categories = [['NA', 'Unf', 'RFn', 'Fin']]
 paveddrive_categories = [['N', 'P', 'Y']]
-fence_privacy_categories = [['NA', 'MnWw', 'GdWo', 'MnPrv', 'GdPrv']]
-fence_wood_categories = [['NA', 'GdPrv', 'MnPrv', 'MnWw', 'GdWo']]
+
 
 # Criar os encoders
 lotshape_encoder = OrdinalEncoder(categories=lotshape_categories)
@@ -48,8 +46,7 @@ bsmtexposure_encoder = OrdinalEncoder(categories=bsmtexposure_categories)
 bsmtfintype_encoder = OrdinalEncoder(categories=bsmtfintype_categories)
 garagefinish_encoder = OrdinalEncoder(categories=garagefinish_categories)
 paveddrive_encoder = OrdinalEncoder(categories=paveddrive_categories)
-fence_privacy_encoder = OrdinalEncoder(categories=fence_privacy_categories)
-fence_wood_encoder = OrdinalEncoder(categories=fence_wood_categories)
+
 
 # Aplicar os encoders
 data_train['LotShape'] = lotshape_encoder.fit_transform(data_train[['LotShape']])
@@ -57,8 +54,7 @@ data_train['LandSlope'] = landslope_encoder.fit_transform(data_train[['LandSlope
 data_train['BsmtExposure'] = bsmtexposure_encoder.fit_transform(data_train[['BsmtExposure']])
 data_train['GarageFinish'] = garagefinish_encoder.fit_transform(data_train[['GarageFinish']])
 data_train['PavedDrive'] = paveddrive_encoder.fit_transform(data_train[['PavedDrive']])
-data_train['Fence_Privacy'] = fence_privacy_encoder.fit_transform(data_train[['Fence']])
-data_train['Fence_Wood'] = fence_wood_encoder.fit_transform(data_train[['Fence']])
+
 
 # Aplicar os mesmos encoders aos dados de teste
 data_test['LotShape'] = lotshape_encoder.transform(data_test[['LotShape']])
@@ -66,8 +62,7 @@ data_test['LandSlope'] = landslope_encoder.transform(data_test[['LandSlope']])
 data_test['BsmtExposure'] = bsmtexposure_encoder.transform(data_test[['BsmtExposure']])
 data_test['GarageFinish'] = garagefinish_encoder.transform(data_test[['GarageFinish']])
 data_test['PavedDrive'] = paveddrive_encoder.transform(data_test[['PavedDrive']])
-data_test['Fence_Privacy'] = fence_privacy_encoder.transform(data_test[['Fence']])
-data_test['Fence_Wood'] = fence_wood_encoder.transform(data_test[['Fence']])
+
 
 # Ajustar e transformar 'BsmtFinType1'
 data_train['BsmtFinType1'] = bsmtfintype_encoder.fit_transform(data_train[['BsmtFinType1']])
@@ -91,6 +86,8 @@ encoder2 = OrdinalEncoder(categories=categories2, handle_unknown= 'use_encoded_v
 data_train[list_ordinal2] = encoder2.fit_transform(data_train[list_ordinal2])
 data_test[list_ordinal2] = encoder2.transform(data_test[list_ordinal2])
 
+# Transformar as variáveis categóricas em numéricas não ordenadas em binárias
+
 le = LabelEncoder()
 
 for col in list_lb:
@@ -103,7 +100,6 @@ column_trans = make_column_transformer(
     remainder='passthrough'
 )
 
-
 # Aplicar a transformação aos dados de treinamento
 data_train_encoded = column_trans.fit_transform(data_train)
 data_test_encoded = column_trans.transform(data_test) 
@@ -115,11 +111,8 @@ test = pd.DataFrame(data_test_encoded,columns = column_trans.get_feature_names_o
 
 train['SalePrice'] = y_train
 
-train.drop(columns=['remainder__Fence'], inplace=True)
-test.drop(columns=['remainder__Fence'], inplace=True)
-
-train = train.fillna('NA')
-test = test.fillna('NA')
+train.fillna('NA', inplace=True)
+test.fillna('NA', inplace=True)
 
 train.to_csv('dataset/train_encoded.csv', index=False)
 test.to_csv('dataset/test_encoded.csv', index=False)
