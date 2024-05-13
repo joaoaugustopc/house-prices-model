67a359bac5d91e5aa82380b18e75e2285cba616b
Luíza Costa <117786812+luizamc003@users.noreply.github.com>
2024-05-13 14:57:52 -0300

    testando diferentes modelos

--- model/models/symb_reg.py
+++ model/models/symb_reg.py
@@ -2,18 +2,17 @@ import pandas as pd
 import numpy as np
 from gplearn.genetic import SymbolicRegressor
 from gplearn.functions import make_function
-from sklearn import set_config
 from sklearn.model_selection import GridSearchCV
 
-data_train = pd.read_csv('data/pre_data_train.csv')
-data_test = pd.read_csv('data/pre_data_test.csv')
+data_train = pd.read_csv('dataset/train_scaled.csv')
+data_test = pd.read_csv('dataset/test_scaled.csv')
 
 
 target = "SalePrice"
 
-X_train = data_train.drop(["Id", "SalePrice"], axis=1)
+X_train = data_train.drop(["remainder__remainder__Id", "SalePrice"], axis=1)
 y_train = data_train[target]
-X_test = data_test.drop(["Id"], axis=1).select_dtypes(include=["number"])
+X_test = data_test.drop(["remainder__remainder__Id"], axis=1).select_dtypes(include=["number"])
 
 #define operacoes protegidas
 def _protected_div(x1, x2):
@@ -42,13 +41,37 @@ protected_sqrt = make_function(function=_protected_sqrt, name='protected_sqrt', 
 protected_log = make_function(function=_protected_log, name='protected_log', arity=1)
 
 function_set = ['add', 'sub', 'mul', 'cos', 'sin', 'tan', protected_div, protected_sqrt, protected_log]
-
-est_gp = SymbolicRegressor(population_size=1000,generations= 50, tournament_size= 6, function_set=function_set,metric='mse', verbose= 1,p_crossover=0.9, init_depth=(5, 10),p_point_mutation=0.01, p_subtree_mutation=0.01, p_hoist_mutation=0.01,parsimony_coefficient=1000.0)
+est_gp = SymbolicRegressor(population_size=500,generations= 50, tournament_size= 10, function_set=function_set,metric='mse', verbose= 1,p_crossover=0.9, init_depth=(5, 10),p_point_mutation=0.01, p_subtree_mutation=0.01, p_hoist_mutation=0.01, parsimony_coefficient=0.001)
 
 est_gp.fit(X_train, y_train)
+"""
+
+est_gp = SymbolicRegressor()    
+
+param_grid = {
+    'population_size': [100, 500],
+    'generations': [50],
+    'tournament_size': [5, 10],
+    'function_set': [function_set],
+    'metric': ['mse'],
+    'p_crossover': [0.9],
+    'init_depth': [(5, 10)],
+    'p_point_mutation': [0.01],
+    'p_subtree_mutation': [0.01],
+    'p_hoist_mutation': [0.01],
+    'parsimony_coefficient': [0.001, 0.1]
+}
+
+grid_search = GridSearchCV(estimator=est_gp, param_grid=param_grid, cv=10, n_jobs=-1, verbose=2)
+grid_search.fit(X_train, y_train)
+
+print("Melhores parâmetros:", grid_search.best_params_)
+print("Melhor score:", grid_search.best_score_)
+""" 
 
 y_pred = est_gp.predict(X_test)
 
-result = pd.DataFrame({'Id': data_test['Id'], 'SalePrice': y_pred})
+result = pd.DataFrame({'Id': data_test['remainder__remainder__Id'], 'SalePrice': y_pred})
+result['Id'] = result['Id'].astype(int)
 
-result.to_csv('data/sample_submission_gp.csv', index=False)
+result.to_csv('submissions/sample_submission_gp_scaled.csv', index=False)
